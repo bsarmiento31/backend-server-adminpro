36aefbe469474122ed53157c56081b3eb2b2d19d
brayan <bsarmientoparada@gmail.com>
2019-12-14 00:13:39 -0500

    Usuario y token listo

---
+++ routes/login.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var bcrypt = require('bcryptjs');
+var Usuario = require('../models/usuario');
+var jwt = require('jsonwebtoken');
+var SEED = require('../config/config').SEED;
+
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuarios',
+                errors: err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales Incorrectas - email',
+                errors: err
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales Incorrectas - password',
+                errors: err
+            });
+        }
+
+        //Crear un token
+        usuarioDB.password = ':)';
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); //4 horas
+
+
+        res.status(200).json({
+            ok: true,
+            Usuario: usuarioDB,
+            token: token,
+            id: usuarioDB.id
+        });
+
+
+    });
+
+})
+
+module.exports = app;
