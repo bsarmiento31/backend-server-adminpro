cdacf23c6b5621044a190aaa1da802c013637296
brayan <bsarmientoparada@gmail.com>
2019-12-24 20:06:09 -0500

    Session terminada google signig 12

---
+++ README.md
@@ -0,0 +1,7 @@
+#Backend-server
+Este es el codigo necesario para establecer el backend de la app adminpro con mongoDB
+
+```
+npm install
+
+```
--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//Googñe 
+module.exports.CLIENT_ID = '682580308324-mo8b71p1enrpuu0mbktr72lbe91cgncn.apps.googleusercontent.com';
